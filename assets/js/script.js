8e282935c233e5e09450fd63b93adca33d075d8c
nhodges <hodges27@outlook.com>
2022-02-09 16:11:45 -0500

    updated hour js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,60 @@
+$(document).ready(function() {
+  // save button clicks
+  $('.saveBtn').on('click', function() {
+    // get values
+    var value = $(this)
+      .siblings('.description')
+      .val();
+    var time = $(this)
+      /parent()
+      .attr('id');
+
+    // save in localStorage
+    localStorage.setItem(time, value);
+
+    // item saved notification
+    $('.notification').addClass('show');
+
+    // remove 'show' class after 5 sec
+    setTimeout(function() {
+      $('.notification').removeClass('show');
+    }, 5000);
+  });
+
+  function hourUpdater() {
+    // current number of hours
+    var currentHour = moment().hours();
+
+    // loop over time blocks
+    $('.time-block').each(function() {
+      var blockHour = parseInt(
+        $(this)
+          .attr('id')
+          .split('-')[1]
+      );
+
+      // check if time has past
+      if (blockHour < currentHour) {
+        $(this).addClass('past');
+      } else if (blockHour === currentHour) {
+        $(this).removeClass('past');
+        $(this).addClass('present');
+      } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+      }
+    });
+  }
+  
+})
+
+
+// display current day on page
+$('#currentDay').text(moment().format('dddd, MMMM Do'));
+
+
+
 var tasks = {};
 
 $("#due").click("div", function() {
